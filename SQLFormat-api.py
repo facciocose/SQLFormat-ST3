f7aed74c68f5189a6388ffc99af20bf348f0f9c9
Luca Marra <ldm.marra@gmail.com>
2013-05-30 14:56:25 +0300

    reformat all the buffer if selection is empty

--- SQLFormat-api.py
+++ SQLFormat-api.py
@@ -6,17 +6,20 @@ import json
 class SqlformatCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
-            if not region.empty():
-                sql = self.view.substr(region)
-                params = {
-                    'sql': sql,
-                    'reindent': 1,
-                    'indent_width': 4,
-                    'identifier_case': 'lower',
-                    'keyword_case': 'upper'
-                }
-                data = urlencode(params)
-                response = urlopen('http://sqlformat.org/api/v1/format', data=data.encode('utf8'))
-                data = json.loads(response.read().decode('utf8'))
-                result = data['result']
-                self.view.replace(edit, region, result)
+            if region.empty():
+                region = sublime.Region(0, self.view.size())
+
+            sql = self.view.substr(region)
+            params = {
+                'sql': sql,
+                'reindent': 1,
+                'indent_width': 4,
+                'identifier_case': 'lower',
+                'keyword_case': 'upper',
+                'strip_comments': 1
+            }
+            data = urlencode(params)
+            response = urlopen('http://sqlformat.org/api/v1/format', data=data.encode('utf8'))
+            data = json.loads(response.read().decode('utf8'))
+            result = data['result']
+            self.view.replace(edit, region, result)
